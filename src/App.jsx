8ff56159f76ef5fd79349a35466b20055afe7a40
DELL-MINA\love8 <mina@bonvies.com>
2024-09-04 18:36:32 +0800

    display member list

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,56 @@ import axios from "axios";
 
 function App() {
   // const [count, setCount] = useState(0);
-  const [meals, setMeals] = useState([]);
+  const [members, setMembers] = useState([]);
+
+  const KEY = "WV83JRA9h4rUeRFx";
+  const SECRET = "aAcraCU4VMLiqTSa";
+  const API = "https://telesale-drvet-api-nygimqgkxq-de.a.run.app/service";
+
+  const phoneBy3cx = "88665683";
+  const rightPhoneLength = "8";
 
   useEffect(() => {
     axios
-      .get("https://www.themealdb.com/api/json/v1/1/random.php")
+      .get(
+        `${API}/member?phoneBy3cx=${phoneBy3cx}&rightPhoneLength=${rightPhoneLength}`,
+        {
+          headers: {
+            "x-api-key": KEY,
+            "x-api-secret": SECRET,
+          },
+        }
+      )
       .then((res) => {
         console.log(res);
-        setMeals(res.data.meals)
+        setMembers(res.data.list);
       });
   }, []);
 
   return (
     <>
-    
-      {meals.map(
-        (meal)=>(<img key={meal.idMeal} src={meal.strMealThumb} alt={meal.strMeal} width={400}/>)
-      )}
+    <table>
+            <thead>
+              <tr>
+                <th>客戶名稱</th>
+                <th>電話</th>
+                <th>市話</th>
+                <th>公司電話</th>
+              </tr>
+            </thead>
+      {members.map((member) => (
+        <>
+            <tbody key={member.id}>
+              <tr>
+                <td>{member.memberName}</td>
+                <td>{member.phone}</td>
+                <td>{member.localPhone}</td>
+                <td>{member.companyPhone}</td>
+              </tr>
+            </tbody>
+        </>
+      ))}
+      </table>
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
